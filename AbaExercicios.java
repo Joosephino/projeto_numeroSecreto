e39bf6f1cfa2a6c8ba3ac7a64f15317afe3d300b
Marco TOrtelli <marcopazinitortelli@gmail.com>
2024-06-13 10:47:53 -0300

    Teste Switch Case

--- AbaExercicios.java
+++ AbaExercicios.java
@@ -1,29 +1,22 @@
-import java.util.Random;
-import java.util.Scanner;
-
-public class AbaExercicios {
+public class AbaExercicios{
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in);
-        int numeroAleatorio = new Random().nextInt(100);
-        int numeroInserido = 0;
-        int tentativas = 0;
+        int dia = 3;
+        String nomeDoDia;
 
-        while (tentativas < 5){
-            System.out.println("Digite um número:");
-            numeroInserido = leitura.nextInt();
-            tentativas++;
-
-            if (numeroInserido == numeroAleatorio){
-                System.out.println("Parabéns! Você acertou o número secreto");
+        switch (dia) {
+            case 1:
+                nomeDoDia = "Segunda Feira";
                 break;
-            } else if (numeroInserido < numeroAleatorio){
-                System.out.println("O número informado é menor que o número gerado");
-            } else{
-                System.out.println("O número informado é maior que o número gerado");
-            }
-            if (tentativas == 5 && numeroInserido != numeroAleatorio) {
-                System.out.println("Tentativas esgotadas :(, o número gerado era: " + numeroAleatorio);
-            }
+            case 2:
+                nomeDoDia = "Terça Feira";
+                break;
+            case 3:
+                nomeDoDia = "Quarta Feira";
+                break;
+            default:
+                nomeDoDia = "Dia inválido";
+                break;
         }
+        System.out.println("O dia " + dia + " na semana é: " + nomeDoDia);
     }
 }
